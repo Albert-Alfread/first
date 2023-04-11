a384f76a4d250f3297daa2485e8aa719803a87b7
褚一杰 <1348467482@qq.com>
2023-04-11 08:33:09 +0800

    初始提交

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class Hello{
+	public static void main(String [] args){
+		System.out.println("hello word line 1")
+		System.out.println("hello word line 2")
+		System.out.println("hello word line 3")
+		System.out.println("hello word line 4")
+		System.out.println("hello word line 5")
+		System.out.println("hello word line 6")
+	
+	}
+
+}
